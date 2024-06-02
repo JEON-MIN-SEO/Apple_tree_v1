5b8dfb22a19f0f13eebd677783834ad14d87264d
jmsparrowk <jmsparrowk@naver.com>
2024-06-02 17:33:09 +0900

    6/2

--- src/main/java/mate/apple_tree_reservation/controller/api/API_ReservationController.java
+++ src/main/java/mate/apple_tree_reservation/controller/api/API_ReservationController.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/reservations")
+@RequestMapping("/reservation")
 public class API_ReservationController {
 
     private final ReservationService reservationService;
