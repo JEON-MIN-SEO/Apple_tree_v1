bc784f46f327b4d126b1e8e5342ba23140568025
jmsparrowk <jmsparrowk@naver.com>
2024-06-03 22:28:36 +0900

    6/3

--- src/main/java/mate/apple_tree_reservation/config/WebConfig.java
+++ src/main/java/mate/apple_tree_reservation/config/WebConfig.java
@@ -10,11 +10,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081","http://localhost:3000",
-                        "https://port-0-apple-tree-v1-1mrfs72llwuqd2yb.sel5.cloudtype.app") // 허용할 출처
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("*") // 허용할 헤더
-                .allowCredentials(true) // 쿠키 인증 요청 허용
-                .maxAge(3600); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱 (초 단위)
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://port-0-apple-tree-v1-1mrfs721lwuqd2yb.sel5.cloudtype.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*") // 모든 헤더 허용
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
