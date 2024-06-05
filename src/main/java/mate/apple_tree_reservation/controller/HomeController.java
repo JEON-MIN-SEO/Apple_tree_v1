46b063fc73a5217ada76ad0f2087c8e4fbbdb80f
jmsparrowk <jmsparrowk@naver.com>
2024-06-05 19:06:36 +0900

    6/5

--- src/main/java/mate/apple_tree_reservation/controller/HomeController.java
+++ src/main/java/mate/apple_tree_reservation/controller/HomeController.java
@@ -2,12 +2,13 @@ package mate.apple_tree_reservation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/";
     }
 }
